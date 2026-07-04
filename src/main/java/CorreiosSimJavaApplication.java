591326200810a7c5e28ad5855f098b5998fa04da
Pietro Zanotim <pietroalveszanotim@gmail.com>
2026-07-04 14:24:46 -0300

    Initial commit

---
+++ src/main/java/CorreiosSimJavaApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosSimJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreiosSimJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com.group.CorreiosSimJava.config/Config.java
@@ -0,0 +1,11 @@
+package com.group.CorreiosSimJava.config;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Config implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
