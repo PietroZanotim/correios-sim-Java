0b530c3859bfc6092fd2fcd37744be3771defdc4
Pietro Zanotim <pietroalveszanotim@gmail.com>
2026-07-18 14:34:33 -0300

    Exclusão de exceptions

--- src/main/java/com/group/CorreiosSimJava/exceptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.group.CorreiosSimJava.exceptions;
-
-public class InvalidEmailException extends RuntimeException {
-    public InvalidEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/group/CorreiosSimJava/exceptions/InvalidNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.group.CorreiosSimJava.exceptions;
-
-public class InvalidNumberException extends RuntimeException {
-    public InvalidNumberException(String message) {
-        super(message);
-    }
-}
